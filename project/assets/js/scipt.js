7dc357e5d6c2f78be3edb259346eab0d75c04315
Caborca <pilarbuchen@gmail.com>
2021-02-21 18:40:10 -0600

    added another functionality to the form

--- project/assets/js/scipt.js
+++ project/assets/js/scipt.js
@@ -1,12 +1,22 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+
 var createTaskHandler = function (event) {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    var taskInfoE1 = document.createElement("div");
+    taskInfoE1.className = "task-info";
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    
+    listItemEl.appendChild(taskInfoE1)
     tasksToDoEl.appendChild(listItemEl);
+
 };
 
-formE1.addEventListener("click", createTaskHandler);
+formE1.addEventListener("submit", createTaskHandler);
