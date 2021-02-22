d8e6fff3a7ae0cf685b7809fe54ea1581ee10eba
Caborca <pilarbuchen@gmail.com>
2021-02-21 18:51:57 -0600

    separating the code into two functions

--- project/assets/js/scipt.js
+++ project/assets/js/scipt.js
@@ -1,22 +1,29 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-
-var createTaskHandler = function (event) {
+var taskFormHandler = function (event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    
+
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskE1(taskDataObj); 
+};
+
+var createTaskE1 = function (taskDataObj) {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
     var taskInfoE1 = document.createElement("div");
     taskInfoE1.className = "task-info";
-    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-    
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+
     listItemEl.appendChild(taskInfoE1)
     tasksToDoEl.appendChild(listItemEl);
+}
 
-};
-
-formE1.addEventListener("submit", createTaskHandler);
+formE1.addEventListener("submit", taskFormHandler);
